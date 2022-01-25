907b2d831bfbbb44be2a2876e592ee0a664a596f
rahulbora <rahul.b@antino.io>
2022-01-25 09:56:02 +0530

    created global filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { BasicTable } from "./components/BasicTable";
+import { SortingTable } from "./components/SortingTable";
 
 function App() {
   return (
     <div className="App">
-      <BasicTable />
+      {/* <BasicTable /> */}
+      {/* <SortingTable /> */}
+      <GlobalFilter />
     </div>
   );
 }
---
+++ src/components/Filter.js
@@ -0,0 +1,51 @@
+import React, { useMemo } from "react";
+import { useTable, useGlobalFilter } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+
+export const Filter = () => {
+
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter
+  );
+  const { globalFilter } = state;
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
